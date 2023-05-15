e81e9fd72342493de3dcc8216250599be2e72ba2
Francesco Dente s313355 <s313355@studenti.polito.it>
2023-05-15 18:03:56 +0200

    test(auth): added unit test suite for registerAdmin

--- code/test/auth.unit.test.js
+++ code/test/auth.unit.test.js
@@ -2,7 +2,7 @@ import request from 'supertest';
 import { app } from '../app';
 import { User } from '../models/User.js';
 import jwt from 'jsonwebtoken';
-import { register } from '../controllers/auth';
+import { register, registerAdmin } from '../controllers/auth';
 const bcrypt = require("bcryptjs")
 
 
@@ -87,8 +87,78 @@ describe('register', () => {
 });
 
 describe("registerAdmin", () => {
-    test('Dummy test, change it', () => {
-        expect(true).toBe(true);
+    // Define the request and response objects
+    let req;
+    let res;
+
+    beforeEach(() => {
+        req = {
+            body: {
+                username: 'testadmin',
+                email: 'admin@example.com',
+                password: 'adminpassword',
+            },
+        };
+        res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should register a new admin user and return a success message', async () => {
+        // Mock the findOne function to return null (no existing user)
+        User.findOne.mockResolvedValue(null);
+        // Mock the hash function to return the hashed password
+        bcrypt.hash.mockResolvedValue('hashedpassword');
+        // Mock the create function to return the newly created user
+        User.create.mockResolvedValue({
+            username: 'testadmin',
+            email: 'admin@example.com',
+            password: 'hashedpassword',
+            role: 'Admin',
+        });
+
+        await registerAdmin(req, res);
+
+        // Check if the appropriate functions were called
+        expect(User.findOne).toHaveBeenCalledWith({ email: 'admin@example.com' });
+        expect(bcrypt.hash).toHaveBeenCalledWith('adminpassword', 12);
+        expect(User.create).toHaveBeenCalledWith({
+            username: 'testadmin',
+            email: 'admin@example.com',
+            password: 'hashedpassword',
+            role: 'Admin',
+        });
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith('admin added succesfully');
+    });
+
+    it('should return an error message if the admin user is already registered', async () => {
+        // Mock the findOne function to return an existing user
+        User.findOne.mockResolvedValue({ username: 'testadmin', email: 'admin@example.com' });
+
+        await registerAdmin(req, res);
+
+        // Check if the appropriate functions were called
+        expect(User.findOne).toHaveBeenCalledWith({ email: 'admin@example.com' });
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({ message: 'you are already registered' });
+    });
+
+    it('should return an error if an exception occurs', async () => {
+        // Mock the findOne function to throw an error
+        User.findOne.mockRejectedValue(new Error('Database error'));
+
+        await registerAdmin(req, res);
+
+        // Check if the appropriate functions were called
+        expect(User.findOne).toHaveBeenCalledWith({ email: 'admin@example.com' });
+        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.json).toHaveBeenCalledWith(new Error('Database error'));
     });
 })
 
