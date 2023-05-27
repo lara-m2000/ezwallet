af979a89f7d0d007a8d0b0ccddff839e6645bcda
Francesco Dente s313355 <s313355@studenti.polito.it>
2023-05-27 17:31:24 +0200

    feat(auth): new input control on auth.js functions

--- code/controllers/auth.js
+++ code/controllers/auth.js
@@ -6,13 +6,13 @@ import { verifyAuth } from './utils.js';
 export const isValidEmail = (email) => {
     // Regular expression pattern for validating email addresses
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    
+
     // Test the email against the regex pattern
     return emailRegex.test(email);
-  }
+}
 
 export const isValidBody = (body) => {
-    if (body.username && body.email && body.password){
+    if (body.username && body.email && body.password) {
         return true;
     }
     else {
@@ -30,13 +30,13 @@ export const register = async (req, res) => {
     try {
         const { username, email, password } = req.body;
         if (!isValidBody(req.body)) {
-            return res.status(400).json({error: "Non valid req.body"});
+            return res.status(400).json({ error: "Non valid req.body" });
         }
-        if (!isValidEmail(email)){
-            res.status(400).json({error: "email is not in the correct format"});
+        if (!isValidEmail(email)) {
+            res.status(400).json({ error: "email is not in the correct format" });
             return;
         }
-        const existingUser = await User.findOne({ $or: [{email: email}, {username: username}] });
+        const existingUser = await User.findOne({ $or: [{ email: email }, { username: username }] });
         if (existingUser) return res.status(400).json({ error: "you are already registered" });
         const hashedPassword = await bcrypt.hash(password, 12);
         const newUser = await User.create({
@@ -44,9 +44,9 @@ export const register = async (req, res) => {
             email,
             password: hashedPassword,
         });
-        res.status(200).json({data:{message:'user added succesfully'}});
+        res.status(200).json({ data: { message: 'user added succesfully' } });
     } catch (err) {
-        res.status(500).json({error: err.message});
+        res.status(500).json({ error: err.message });
     }
 };
 
@@ -59,12 +59,15 @@ export const register = async (req, res) => {
  */
 export const registerAdmin = async (req, res) => {
     try {
+        if (!isValidBody(req.body)) {
+            return res.status(400).json({ error: "Non valid req.body" });
+        }
         const { username, email, password } = req.body
-        if (!isValidEmail(email)){
-            res.status(400).json({error: "email is not in the correct format"});
+        if (!isValidEmail(email)) {
+            res.status(400).json({ error: "email is not in the correct format" });
             return;
         }
-        const existingUser = await User.findOne({ $or: [{email: email}, {username: username}] });
+        const existingUser = await User.findOne({ $or: [{ email: email }, { username: username }] });
         if (existingUser) return res.status(400).json({ error: "you are already registered" });
         const hashedPassword = await bcrypt.hash(password, 12);
         const newUser = await User.create({
@@ -73,9 +76,9 @@ export const registerAdmin = async (req, res) => {
             password: hashedPassword,
             role: "Admin"
         });
-        res.status(200).json({data:{message:'admin added succesfully'}});
+        res.status(200).json({ data: { message: 'admin added succesfully' } });
     } catch (err) {
-        res.status(500).json({error: err.message});
+        res.status(500).json({ error: err.message });
     }
 
 }
@@ -89,13 +92,22 @@ export const registerAdmin = async (req, res) => {
     - error 400 is returned if the supplied password does not match with the one in the database
  */
 export const login = async (req, res) => {
-    const { email, password } = req.body
-    const cookie = req.cookies
     try {
+        const { email, password } = req.body;
+        const body = req.body;
+        //Dummy property to use isValidBody also for login
+        body.username = body.email;
+        if (!isValidBody(body)) {
+            return res.status(400).json({ error: "Non valid req.body" });
+        }
+        if (!isValidEmail(email)) {
+            res.status(400).json({ error: "email is not in the correct format" });
+            return;
+        }
         const existingUser = await User.findOne({ email: email })
-    if (!existingUser) return res.status(400).json({error:'please you need to register'})
+        if (!existingUser) return res.status(400).json({ error: 'please you need to register' })
         const match = await bcrypt.compare(password, existingUser.password)
-        if (!match) return res.status(400).json({error:'wrong credentials'})
+        if (!match) return res.status(400).json({ error: 'wrong credentials' })
         //CREATE ACCESSTOKEN
         const accessToken = jwt.sign({
             email: existingUser.email,
@@ -117,7 +129,7 @@ export const login = async (req, res) => {
         res.cookie('refreshToken', refreshToken, { httpOnly: true, domain: "localhost", path: '/api', maxAge: 7 * 24 * 60 * 60 * 1000, sameSite: 'none', secure: true })
         res.status(200).json({ data: { accessToken: accessToken, refreshToken: refreshToken } })
     } catch (error) {
-        res.status(500).json({error: error.message})
+        res.status(500).json({ error: error.message })
     }
 }
 
@@ -131,16 +143,16 @@ export const login = async (req, res) => {
  */
 export const logout = async (req, res) => {
     const refreshToken = req.cookies.refreshToken
-    if (!refreshToken) return res.status(400).json({error:"user not found"})
+    if (!refreshToken) return res.status(400).json({ error: "user not found" })
     try {
         const user = await User.findOne({ refreshToken: refreshToken })
-        if (!user) return res.status(400).json({error:'user not found'})
+        if (!user) return res.status(400).json({ error: 'user not found' })
         user.refreshToken = null
         res.cookie("accessToken", "", { httpOnly: true, path: '/api', maxAge: 0, sameSite: 'none', secure: true })
         res.cookie('refreshToken', "", { httpOnly: true, path: '/api', maxAge: 0, sameSite: 'none', secure: true })
         const savedUser = await user.save()
-        res.status(200).json({data:{message:'logged out'}})
+        res.status(200).json({ data: { message: 'logged out' } })
     } catch (error) {
-        res.status(500).json({error: error.message})
+        res.status(500).json({ error: error.message })
     }
 }
